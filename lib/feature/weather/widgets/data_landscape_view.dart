403ba2f24e953a9dc9f22973b39a450505977d0c
Mohsen <mohsen.namazi.job@gmail.com>
2024-06-30 19:03:12 +0200

    style: improve tablet UI

--- lib/feature/weather/widgets/data_landscape_view.dart
+++ lib/feature/weather/widgets/data_landscape_view.dart
@@ -14,60 +14,75 @@ class _DataLandscapeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
-    final screenWidth = context.width;
     final appUnit = inject<UnitsCubit>().state;
 
-    return Stack(
+    return ListView(
       children: [
         Padding(
-          padding: const EdgeInsets.all(Spacing.s4).copyWith(
-            left: screenWidth / 3 + Spacing.s4,
-          ),
+          padding: const EdgeInsets.all(Spacing.s4),
           child: Row(
             children: [
               Expanded(
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(l10n.humidity(selectedDay.first.main.humidity)),
+                    Center(child: Text(selectedDay.first.weather.first.main)),
                     const SizedBox(height: Spacing.s4),
-                    Text(l10n.pressure(selectedDay.first.main.pressure)),
+                    Wrap(
+                      children: [
+                        Text(l10n.humidity(selectedDay.first.main.humidity)),
+                        const SizedBox(width: Spacing.s4),
+                        Text(l10n.pressure(selectedDay.first.main.pressure)),
+                        const SizedBox(width: Spacing.s4),
+                        Text(l10n.wind(selectedDay.first.wind.speed)),
+                      ],
+                    ),
                     const SizedBox(height: Spacing.s4),
-                    Text(l10n.wind(selectedDay.first.wind.speed)),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: selectedDay
+                            .map(
+                              (weatherData) => Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: Spacing.s2),
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      DateFormat('HH:mm')
+                                          .format(weatherData.dateTime),
+                                    ),
+                                    const SizedBox(
+                                      height: Spacing.s1,
+                                    ),
+                                    Text(
+                                      '${weatherData.main.temp} ${appUnit.sym}',
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            )
+                            .toList(),
+                      ),
+                    ),
                   ],
                 ),
               ),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(selectedDay.first.weather.first.main),
-                    const SizedBox(height: Spacing.s4),
-                    AspectRatio(
-                      aspectRatio: 4 / 3,
-                      child: ColoredBox(
-                        color: Colors.black.withOpacity(.5),
-                        child: FittedBox(
-                          child: WeatherImage(
-                            iconId: selectedDay.first.weather.first.icon,
-                          ),
-                        ),
-                      ),
+              SizedBox(
+                width: Spacing.s40,
+                child: ColoredBox(
+                  color: Colors.black.withOpacity(.5),
+                  child: FittedBox(
+                    child: WeatherImage(
+                      iconId: selectedDay.first.weather.first.icon,
                     ),
-                    const SizedBox(height: Spacing.s4),
-                    Text(
-                      '${selectedDay.first.main.temp} ${appUnit.sym}',
-                    ),
-                  ],
+                  ),
                 ),
-              )
+              ),
             ],
           ),
         ),
-        ListView(
-          padding: const EdgeInsets.all(Spacing.s4)
-              .copyWith(right: screenWidth * 2 / 3),
+        const SizedBox(height: Spacing.s4),
+        Row(
           children: [
             ...data.groupedDays.map((groupedDay) {
               final isTileSeleced = groupedDay.first.dateTime.day ==
